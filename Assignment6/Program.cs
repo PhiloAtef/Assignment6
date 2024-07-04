ad5fc6ecf2b3cbacfb3cfe224f9e288706cb025c
Philo <philoatefdev@gmail.com>
2024-07-04 21:51:11 +0300

    ALL QUESTIONS

--- Assignment6/Program.cs
+++ Assignment6/Program.cs
@@ -2,14 +2,9 @@
 
 namespace Assignment6
 {
-    enum Weekdays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
-    enum Season { Spring, Summer, Autumn, Winter }
-    enum Colors { Red, Green, Blue }
-    
-    [Flags]
-    enum Permissions
-    { Read = 1, Write = 2, Delete = 4, Execute = 8 }
 
+    #region Q1
+    enum Weekdays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
     class WeekDaysEnum
     {
         public static void PrintAllWeekDays()
@@ -20,7 +15,9 @@ namespace Assignment6
             }
         }
     }
+    #endregion
 
+    #region Q2 AND Q7
     struct Person
     {
         public string Name { get; set; }
@@ -39,6 +36,44 @@ namespace Assignment6
             persons[2] = new Person { Name = "ahmed", Age = 26 };
         }
 
+        public PersonManager(int countOfPersons)
+        {
+            if (countOfPersons >= 0)
+            {
+                persons = new Person[countOfPersons];
+
+                //code here can be refactored into a function that returns a persons array with the logic here
+                for (int i = 0; i < persons.Length; i++)
+                {
+                    Console.WriteLine($"Enter details for person no.{i+1}");
+                    Console.Write("Name: ");
+                    string? name = Console.ReadLine();
+                    Console.Write("Age: ");
+                    int age = int.Parse(Console.ReadLine());
+                    persons[i] = new Person { Name =  name ?? "Null", Age = age }; 
+                }
+            }
+            else
+            {
+                persons = Array.Empty<Person>();
+            }
+        }
+
+        public void DisplayOldestPerson()
+        {
+            Person oldestPerson = persons[0];
+
+            foreach (Person person in persons)
+            {
+                if (person.Age > oldestPerson.Age)
+                {
+                    oldestPerson = person;
+                }
+            }
+
+            Console.WriteLine($"Oldest Person's name is {oldestPerson.Name} and age is {oldestPerson.Age}");
+        }
+
         public void DisplayPersons()
         {
             foreach (Person person in persons)
@@ -47,7 +82,10 @@ namespace Assignment6
             }
         }
     }
+    #endregion
 
+    #region Q3
+    enum Season { Spring, Summer, Autumn, Winter }
     class SeasonManager
     {
         private string GetSeasonMonths(Season season)
@@ -82,7 +120,16 @@ namespace Assignment6
             }
         }
     }
+    #endregion
 
+    #region Q4
+    [Flags]
+    enum Permissions
+    { Read = 1, Write = 2, Delete = 4, Execute = 8 } 
+    #endregion
+
+    #region Q5
+    enum Colors { Red, Green, Blue }
     class ColorManager
     {
         public void CheckPrimaryColor()
@@ -104,6 +151,31 @@ namespace Assignment6
             }
         }
     }
+    #endregion
+
+    #region Q6
+    struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    class PointCalculator
+    {
+        public double CalculateDistance(Point point1, Point point2)
+        {
+            double distance = Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+            return distance;
+        }
+
+        public Point RecieveCoordinates(double pointx, double pointy)
+        {
+            return new Point { X = pointx, Y = pointy };
+        }
+    } 
+    #endregion
+
+
 
     internal class Program
     {
@@ -160,9 +232,39 @@ namespace Assignment6
         //}
         #endregion
 
+        #region Q6
+        //static void Main(string[] args)
+        //{
+        //    PointCalculator pointCalculator = new PointCalculator();
 
+        //    Console.WriteLine("Enter first point X-coordinate: ");
+        //    bool point1XParsed = double.TryParse(Console.ReadLine(), out double point1X);
+        //    Console.WriteLine("Enter first point Y-coordinate: ");
+        //    bool point1YParsed = double.TryParse(Console.ReadLine(), out double point1Y);
 
+        //    Point pointX = pointCalculator.RecieveCoordinates(point1X, point1Y);
 
+        //    Console.WriteLine("Enter second point X-coordinate: ");
+        //    bool point2XParsed = double.TryParse(Console.ReadLine(), out double point2X);
+        //    Console.WriteLine("Enter second point y-coordinate: ");
+        //    bool point2YParsed = double.TryParse(Console.ReadLine(), out double point2Y);
+
+        //    Point pointY = pointCalculator.RecieveCoordinates(point2X, point2Y);
+
+        //    double distance = pointCalculator.CalculateDistance(pointX, pointY);
+
+        //    Console.WriteLine($"Distance between point({point1X},{point1Y}) and point({point2X},{point2Y}) is: {distance}");
+
+        //} 
+        #endregion
+
+        #region Q7
+        //static void Main(string[] args)
+        //{
+        //    PersonManager personManager = new PersonManager(3);
+        //    personManager.DisplayOldestPerson();
+        //} 
+        #endregion
 
 
     }
